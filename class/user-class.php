78f5676398e5a8cbc42006e9f1b31cc9c6a7cb75
Leandro Macedo <leandromacedo12@gmail.com>
2016-03-30 02:01:42 -0300

    Commit inicial

---
+++ class/user-class.php
@@ -0,0 +1,42 @@
+<?php
+
+class User
+{
+	private string $name;
+	private string $pass;
+	private string $phone;
+
+
+	public function setName( $name )
+	{
+		$this->name = $name;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setPass( $pass)
+	{
+		$this->pass = $pass;
+	}
+
+	public function getPass()
+	{
+		return $this->pass;
+	}
+
+	public function setPhone( $phone )
+	{
+		$this->phone = $phone;
+	}
+
+	public function getPhone()
+	{
+		return $this->phone;
+	}
+
+}
+
+?>
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("USER-DB", "root");
+define("PASS-DB", "root");
+define("TABLE-DB", "usuario");
+
+
+?>
