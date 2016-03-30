ec6ad4e1e41ed1d5e9860e338e96afacc7565492
Leandro Macedo <leandromacedo12@gmail.com>
2016-03-30 02:40:48 -0300

    Adicionado constante do nome da banco

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ define("USERDB", "root");
 define("PASSDB", "root");
 define("SERVERHOST", "172.17.0.2");
 define("SERVERDB", "172.17.0.2:3364");
+define("DBNAME", "login");
 define("TABLEDB", "usuario");
 
 
